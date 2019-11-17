531fb36005745034d7c7084138a76364d1eea9f6
difelix <dfelixsilva08@gmail.com>
2019-11-16 23:06:57 -0300

    atualizando e corrigindo sql

--- database/init.sql
+++ database/init.sql
@@ -6,32 +6,35 @@ GRANT ALL PRIVILEGES ON DATABASE tabelaJogos TO adminTabelaJogos;
 
 USE tabelaJogos;
 
+CREATE SCHEMA tabelaJogos;
+
 -- guarda dados relacionados ao usuário do sistema
-CREATE TABLE usuario (
-   id_user bigserial PRIMARY KEY,
+CREATE TABLE tabelaJogos.usuario (
+   id bigserial PRIMARY KEY,
    name VARCHAR(30) NOT NULL,
    nickname VARCHAR(10) NOT NULL, -- nome fantasia do usuario
    email VARCHAR(50) NOT NULL,
-   password VARCHAR(8) NOT NULL
+   password VARCHAR(8) NOT NULL,
+   data_criacao date DEFAULT NOW()
 );
 
-CREATE UNIQUE INDEX id_user_idx ON usuario(id_user);
+CREATE UNIQUE INDEX id_user_idx ON tabelaJogos.usuario(id);
 
-CREATE TABLE campeonato (
-   id_campeonato bigserial PRIMARY KEY,
+CREATE TABLE tabelaJogos.campeonato (
+   id bigserial PRIMARY KEY,
    nome VARCHAR(20) NOT NULL,
    organizador VARCHAR(30), -- nome do organizador ou da associação que organiza o campeonato
    temporada VARCHAR(20) NOT NULL, -- ano da realização do campeonato. Ex: 2019/2020
    qntde_times integer NOT NULL,
-   returno boolean NOT NULL, -- define se o campeonato será de turno unico(false) ou de turno e returno(true)
+   returno VARCHAR(20) CHECK (returno IN ('TURNO', 'RETURNO')) NOT NULL, -- define se o campeonato será de turno unico(false) ou de turno e returno(true)
    descricao VARCHAR(100),
-   id_user bigint references usuario -- FK de usuário
+   id_usuario bigint references tabelaJogos.usuario (id) -- FK de usuário
 );
 
-CREATE UNIQUE INDEX id_campeonato_idx ON campeonato(id_campeonato);
+CREATE UNIQUE INDEX id_campeonato_idx ON tabelaJogos.campeonato(id);
 
-CREATE TABLE times (
-   id_time bigserial PRIMARY KEY,
+CREATE TABLE tabelaJogos.times (
+   id bigserial PRIMARY KEY,
    nome VARCHAR(30) NOT NULL,
    data_criacao date NOT NULL,
    cidade_sede VARCHAR(20) NOT NULL, -- cidade onde time atua e manda seus jogos
@@ -40,30 +43,30 @@ CREATE TABLE times (
    descricao VARCHAR(100)
 );
 
-CREATE UNIQUE INDEX id_time_idx ON times(id_time);
+CREATE UNIQUE INDEX id_time_idx ON tabelaJogos.times(id);
 
-CREATE TABLE timeCampeonato (
-   id_timecampeonato bigserial PRIMARY KEY,
-   id_campeonato bigint references campeonato,
-   id_time bigint references times
+CREATE TABLE tabelaJogos.timeCampeonato (
+   id bigserial PRIMARY KEY,
+   id_campeonato bigint references tabelaJogos.campeonato (id),
+   id_time bigint references tabelaJogos.times (id)
 );
 
-CREATE UNIQUE INDEX id_timecampeonato_idx ON timeCampeonato(id_timecampeonato);
+CREATE UNIQUE INDEX id_timecampeonato_idx ON tabelaJogos.timeCampeonato(id);
 
-CREATE TABLE jogador (
-   id_jogador bigserial PRIMARY KEY,
+CREATE TABLE tabelaJogos.jogador (
+   id bigserial PRIMARY KEY,
    nome VARCHAR(30) NOT NULL,
    nascimento date NOT NULL,
    nacionalidade VARCHAR(30) NOT NULL,
    posicao VARCHAR(15) NOT NULL, -- posição aonde o jogador atua em campo. Ex: atacante, meio campo, zagueiro, etc
    descricao VARCHAR(100),
-   id_time bigint references times
+   id_time bigint references tabelaJogos.times (id)
 );
 
-CREATE UNIQUE INDEX id_jogador_idx ON jogador(id_jogador);
+CREATE UNIQUE INDEX id_jogador_idx ON tabelaJogos.jogador(id);
 
-CREATE TABLE jogos (
-   id_jogo bigserial PRIMARY KEY,
+CREATE TABLE tabelaJogos.jogos (
+   id bigserial PRIMARY KEY,
    id_adversario bigint NOT NULL, -- id do time visitante que enfrenta o time da casa
    rodada integer NOT NULL, -- número da rodada do campeonato
    qntde_gols_time integer NOT NULL,
@@ -71,9 +74,9 @@ CREATE TABLE jogos (
    dataJogo date NOT NULL, -- data da realização do jogo
    localJogo VARCHAR(50) NOT NULL, -- local onde o jogo foi realizado
    descricao VARCHAR(100),
-   id_time bigint references times, -- id do time mandante do jogo
-   id_campeonato bigint references campeonato -- id do campeonato  ao qual pertence o jogo
+   id_time bigint references tabelaJogos.times (id), -- id do time mandante do jogo
+   id_campeonato bigint references tabelaJogos.campeonato (id) -- id do campeonato  ao qual pertence o jogo
 );
 
-CREATE UNIQUE INDEX id_jogo_idx ON jogos(id_jogo);
+CREATE UNIQUE INDEX id_jogo_idx ON tabelaJogos.jogos(id);
    
